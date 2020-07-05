f54cf5aec1a4a73108f171fdbeebd26cffcc5d57
Astellarius <zak.lake0@gmail.com>
2020-07-05 15:26:02 -0700

    ready for presentation

--- demo/index.js
+++
@@ -1,9 +0,0 @@
-function alertbox() {
-    alert("ALERT");
-
-    document.getElementById("first").innerHTML = "hello, jarod";
-}
-
-for( let i = 0; i < 100; i++ ) {
-    console.log("hello, world!");
-}
